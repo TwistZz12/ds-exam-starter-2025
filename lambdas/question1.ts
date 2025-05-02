4574508a42bff81c3193cb34abbbb17f4c29714b
TwistZz12 <twistzzmeng@gmail.com>
2025-05-02 11:39:25 +0100

    Revised new endpoint to include query string

--- lambdas/question1.ts
+++ lambdas/question1.ts
@@ -4,7 +4,8 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { 
   DynamoDBDocumentClient, 
   DeleteCommand,
-  GetCommand
+  GetCommand,
+  QueryCommand
 } from "@aws-sdk/lib-dynamodb";
 import { MovieCrewRole } from "../shared/types";
 
@@ -15,39 +16,62 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   try {
     console.log("Event: ", JSON.stringify(event));
 
-    
+    // Handle /crew/{role}/movies/{movieId} endpoint
     if (event.pathParameters && event.pathParameters.role && event.pathParameters.movieId) {
       const role = event.pathParameters.role;
       const movieId = parseInt(event.pathParameters.movieId);
       
-      
-      const params = {
-        TableName: tableName,
-        Key: {
-          movieId: movieId,
-          role: role
-        }
-      };
+      // Check if verbose query parameter is present
+      const verbose = event.queryStringParameters?.verbose === "true";
 
-      const response = await client.send(new GetCommand(params));
-      
-      if (!response.Item) {
+      if (verbose) {
+        // Get all crew members for the movie
+        const params = {
+          TableName: tableName,
+          KeyConditionExpression: "movieId = :movieId",
+          ExpressionAttributeValues: {
+            ":movieId": movieId
+          }
+        };
+
+        const response = await client.send(new QueryCommand(params));
         return {
-          statusCode: 404,
+          statusCode: 200,
           headers: {
             "content-type": "application/json",
           },
-          body: JSON.stringify({ message: `Can't find Movie ID ${movieId}  ${role} ` }),
+          body: JSON.stringify(response.Items),
+        };
+      } else {
+        // Get only the specific role for the movie
+        const params = {
+          TableName: tableName,
+          Key: {
+            movieId: movieId,
+            role: role
+          }
+        };
+
+        const response = await client.send(new GetCommand(params));
+        
+        if (!response.Item) {
+          return {
+            statusCode: 404,
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({ message: `Can't find Movie ID ${movieId} ${role}` }),
+          };
+        }
+        
+        return {
+          statusCode: 200,
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(response.Item),
         };
       }
-      
-      return {
-        statusCode: 200,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(response.Item),
-      };
     }
 
     return {
